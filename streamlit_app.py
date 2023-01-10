2350a62fd1a67c36e75c2fe0cbdaf7e9267c6a17
sfc-gh-tkipkemboi <tony.kipkemboi@snowflake.com>
2023-01-10 17:52:40 -0500

    added tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import pandas_datareader as pdr
 import plotly.graph_objects as go
-import altair as alt
 import streamlit as st
 import datetime as dt
 import pandas as pd
@@ -63,32 +62,33 @@ def main():
             submit = st.form_submit_button(label="Submit")
 
     if submit:
-        stock_data, company_name = get_stock_data(stock, start_date, end_date)
-        st.subheader(company_name)
-        sp_data = get_sp_data(start_date, end_date)
-        stock_return, sp_return = get_returns(stock_data, sp_data)
-        investment_amount = float(investment_amount)
-        simulated_return_delta = investment_amount * stock_return
+        with st.spinner('Loading data...'):
+            stock_data, company_name = get_stock_data(stock, start_date, end_date)
+            st.subheader(company_name)
+            sp_data = get_sp_data(start_date, end_date)
+            stock_return, sp_return = get_returns(stock_data, sp_data)
+            investment_amount = float(investment_amount)
+            simulated_return_delta = investment_amount * stock_return
 
-        with st.container():
-            col1, col2, col3 = st.columns(3)
-            if simulated_return_delta > 0:
-                simulated_return = investment_amount + simulated_return_delta
-                col1.metric(
-                    label="Simulated return",
-                    value=f"${simulated_return:.2f}",
-                    delta=f"${simulated_return_delta:.2f}"
-                )
-            else:
-                if simulated_return_delta < 0:
+            with st.container():
+                col1, col2, col3 = st.columns(3)
+                if simulated_return_delta > 0:
                     simulated_return = investment_amount + simulated_return_delta
                     col1.metric(
                         label="Simulated return",
                         value=f"${simulated_return:.2f}",
                         delta=f"${simulated_return_delta:.2f}"
                     )
-            col2.metric(label=f"{stock} Stock return", value=f"{stock_return:.2%}")
-            col3.metric(label="S&P 500 return", value=f"{sp_return:.2%}")
+                else:
+                    if simulated_return_delta < 0:
+                        simulated_return = investment_amount + simulated_return_delta
+                        col1.metric(
+                            label="Simulated return",
+                            value=f"${simulated_return:.2f}",
+                            delta=f"${simulated_return_delta:.2f}"
+                        )
+                col2.metric(label=f"{stock} Stock return", value=f"{stock_return:.2%}")
+                col3.metric(label="S&P 500 return", value=f"{sp_return:.2%}")
 
         sp_df = sp_data.rename(columns={'High':'SP_High', 'Low':'SP_Low', 'Open':'SP_Open', 
         'Close':'SP_Close', 'Volume':'SP_Volume', 'Adj Close':'SP_Adj_Close'})
@@ -100,13 +100,19 @@ def main():
         sp_return = sp_data_norm['SP_Close'].pct_change()
 
         stock_sp = pd.concat([stock_return, sp_return], axis=1)
+        tab1, tab2, tab3 = st.tabs(['Daily Return', 'Trading Volume', 'Moving Average Chart'])
+        with tab1:
+            st.info(f'{company_name} and S&P 500 Daily Return (normalized returns)')
+            st.bar_chart(stock_sp, use_container_width=True)
+        with tab2:
+            st.info(f'{company_name} Trading Volume')
+            st.bar_chart(stock_data['Volume'], use_container_width=True)
+        with tab3:
+            stock_data['MA10'] = stock_data['Close'].rolling(window=10).mean()
+            st.info('10-day stock moving average for the closing price')
+            st.line_chart(stock_data[['Close', 'MA10']], use_container_width=True)
 
-        st.info(f'{company_name} and S&P 500 Daily Return (normalized returns)')
-        st.bar_chart(stock_sp, use_container_width=True)
+        st.balloons()
 
-        st.info(f'{company_name} Trading Volume')
-        st.bar_chart(stock_data['Volume'], use_container_width=True)
-
-   
 if __name__ == '__main__':
     main()    
